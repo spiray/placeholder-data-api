192d82c4d78265cd115b77fbafd818993e71538d
Yossi Spira <spira.joe214@gmail.com>
2020-11-30 20:31:19 -0500

    api: finished TS API

--- nodejs-ts/.eslintrc.js
+++ nodejs-ts/.eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["airbnb-base", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module'
   },
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error'
   },
+  overrides: [
+    {
+      files: '**/*.ts',
+      rules: {
+        'import/no-unresolved': 0,
+        'import/extensions': 0,
+        'import/prefer-default-export': 0
+      }
+    }
+  ]
 };
--- nodejs-ts/index.ts
+++ nodejs-ts/index.ts
@@ -1,5 +1,72 @@
 import express from 'express';
-import got from 'got';
+import fetch from 'node-fetch';
 import morgan from 'morgan';
+import { Post } from './types';
+import { BASE_URL } from './constants';
 
-console.log({ express, got, morgan });
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.json());
+
+app.get('/posts', async (_, res) => {
+  const response = await fetch(`${BASE_URL}/posts`);
+  const posts: Array<Post> = await response.json();
+
+  return res.json(posts);
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const response = await fetch(`${BASE_URL}/posts/${req.params.id}`);
+  const post: Post = await response.json();
+
+  return res.json(post);
+});
+
+app.post('/posts', async (req, res) => {
+  const { title, body, userId }: Partial<Post> = req.body;
+  const response = await fetch(`${BASE_URL}/posts`, {
+    method: 'POST',
+    body: JSON.stringify({ title, body, userId }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  });
+  const post: Post = await response.json();
+
+  return res.json(post);
+});
+
+app.put('/posts/', async (req, res) => {
+  const { id, title, body, userId }: Post = req.body;
+  const response = await fetch(`${BASE_URL}/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ id, title, body, userId }),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  });
+  const post: Post = await response.json();
+
+  return res.json(post);
+});
+
+app.patch('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const { body }: Partial<Post> = req;
+  const response = await fetch(`${BASE_URL}/posts/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify(body),
+    headers: { 'Content-type': 'application/json; charset=UTF-8' }
+  });
+  const post: Post = await response.json();
+
+  return res.json(post);
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  await fetch(`${BASE_URL}/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  return res.sendStatus(200)
+});
+
+app.listen(3000, () => console.log('Server Started @ http://localhost:3000'));
